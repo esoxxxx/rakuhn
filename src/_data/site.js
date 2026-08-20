e3557312e5981ad4f8c874a0a41e565ca79c8674
Sebastian Kuhn <s6sekuhn+git@gmail.com>
2026-08-20 23:12:07 +0200

    SEO fixes und 404 seite

---
+++ src/_data/site.js
@@ -0,0 +1,7 @@
+module.exports = {
+  url: "https://www.made-by-rakuhn.de",
+  name: "rakuhn by Ina Kuhn",
+  description: "Handgemachte Babymode und Geburtsgeschenke aus Kaiserslautern – Halstücher, Pumphosen, Haarbänder, Windeltaschen und personalisierte Stickerei.",
+  ogImage: "/assets/img/logo/rakuhn-logo.png",
+  wartung: false
+};
